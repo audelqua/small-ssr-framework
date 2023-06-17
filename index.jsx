a30f0b5a1863dde6772c390bc8cc25db791eddbe
Mohammad Bozorgi <m.bozorgi@nizek.com>
2023-06-17 13:30:21 +0330

    note add in index.jsx

--- index.jsx
+++ index.jsx
@@ -4,3 +4,4 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 ReactDOM.hydrate(<App />, document.getElementById('root'))
+// we use hydrate instead of render because it has already been rendered in our server and we just want to hydrate it here. 
