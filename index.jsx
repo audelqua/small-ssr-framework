4e2802224882eab82211f15be06c6c0386213474
Mohammad Bozorgi <m.bozorgi@nizek.com>
2023-06-17 13:32:44 +0330

    note add in index.jsx

--- index.jsx
+++ index.jsx
@@ -5,3 +5,6 @@ import App from './App'
 
 ReactDOM.hydrate(<App />, document.getElementById('root'))
 // we use hydrate instead of render because it has already been rendered in our server and we just want to hydrate it here. 
+
+// what does hydrating means? it means we are going to attach all event handlers and JS codes that are for interaction between user and our page. 
+// so we can not understand that JS code are essential for rendering elements like button size or something like that are not rehydrating and they will get render on initial load to paint our HTML but onClick handler are that actions can send to client later
